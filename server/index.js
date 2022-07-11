6d8145171ce6542815a50d02f8f2568e1536465c
berkbuturak <berkbuturak@gmail.com>
2022-07-11 10:54:36 +0200

    Preparation

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+
+app.get("/users/add", (req, res) => {
+
+
+    res.send({ message: "Add user"});
+});
+
+app.get("/users/remove", (req, res) => {
+
+
+    res.send({ message: "Remove user"});
+});
+
+app.listen(3000, () => console.log("Hamegululu"));
